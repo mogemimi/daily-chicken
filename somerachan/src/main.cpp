9d0a10437349f5e13d146afbf3e336dcf05b1e8f
mogemimi <mogemimi@enginetrouble.net>
2015-12-07 01:41:07 +0900

    Remove unused code and refactor

--- somerachan/src/main.cpp
+++ somerachan/src/main.cpp
@@ -15,21 +15,15 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
-#include "clang/Basic/Diagnostic.h"
-#include "clang/Basic/DiagnosticOptions.h"
 #include "clang/Basic/FileManager.h"
 #include "clang/Basic/SourceLocation.h"
 #include "clang/Basic/SourceManager.h"
 #include "clang/Basic/Version.h"
-#include "clang/Format/Format.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendActions.h"
 #include "clang/Parse/Parser.h"
-#include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Tooling/CommonOptionsParser.h"
-#include "clang/Tooling/Refactoring.h"
 #include "clang/Tooling/Tooling.h"
-
 #include "llvm/ADT/StringMap.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/FileSystem.h"
@@ -37,15 +31,12 @@
 
 #pragma GCC diagnostic pop
 
-#include <algorithm>
-#include <array>
 #include <cassert>
-#include <fstream>
 #include <iostream>
-#include <set>
+#include <memory>
 #include <sstream>
 #include <string>
-#include <vector>
+#include <utility>
 
 using somera::NativeSpellChecker;
 using somera::EditDistance;
@@ -60,8 +51,6 @@ using namespace clang::driver;
 using namespace clang::tooling;
 using namespace llvm;
 
-using clang::tooling::Replacements;
-
 static cl::OptionCategory MyToolCategory("my-tool options");
 static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 static cl::extrahelp MoreHelp("\nMore help text...");
@@ -71,25 +60,20 @@ namespace {
 class MyCommentHandler final : public clang::CommentHandler {
 private:
     llvm::StringRef inputFile;
-    Rewriter* rewriter = nullptr;
     TypoMan* typos = nullptr;
 
 public:
     void setFile(llvm::StringRef fileIn) { inputFile = fileIn; }
 
-    void setRewriter(Rewriter* rewriterIn) { rewriter = rewriterIn; }
-
     void setTypoSet(TypoMan* typosIn) { typos = typosIn; }
 
-    bool HandleComment(clang::Preprocessor &pp, clang::SourceRange range) override
+    bool HandleComment(clang::Preprocessor& pp, clang::SourceRange range) override
     {
-        clang::SourceManager &sm = pp.getSourceManager();
+        clang::SourceManager& sm = pp.getSourceManager();
         if (sm.getFilename(range.getBegin()) != inputFile) {
             return false;
         }
-
         assert(typos != nullptr);
-        assert(rewriter != nullptr);
         assert(sm.getFilename(range.getBegin()) == inputFile);
 
         const auto startLoc = sm.getDecomposedLoc(range.getBegin());
@@ -101,130 +85,25 @@ public:
         source.file = inputFile;
         typos->computeFromSentence(sourceString, source);
 
-//        std::transform(sourceString.begin(), sourceString.end(), sourceString.begin(), toupper);
-//        rewriter->ReplaceText(range, sourceString);
-
         return false;
     }
 };
 
-class IfStmtHandler final : public MatchFinder::MatchCallback {
-public:
-    IfStmtHandler(Rewriter &rewriterIn)
-        : rewriter(rewriterIn)
-    {
-    }
-
-    void run(const MatchFinder::MatchResult &result) override
-    {
-//        if (const IfStmt *ifStatement =
-//                result.Nodes.getNodeAs<clang::IfStmt>("ifStmt")) {
-//            const Stmt *Then = ifStatement->getThen();
-//            rewriter.InsertText(Then->getLocStart(), "// if\n", true, true);
-//
-//            if (const Stmt *Else = ifStatement->getElse()) {
-//                rewriter.InsertText(Else->getLocStart(), "// else\n", true, true);
-//            }
-//        }
-    }
-
-private:
-    Rewriter &rewriter;
-};
-
-class DeclStmtHandler final : public MatchFinder::MatchCallback {
-public:
-    DeclStmtHandler(Rewriter &rewriterIn)
-        : rewriter(rewriterIn)
-    {
-    }
-
-    void run(const MatchFinder::MatchResult &result) override
-    {
-//        if (const DeclStmt *declStatement =
-//            result.Nodes.getNodeAs<clang::DeclStmt>("declStmt")) {
-//
-//            const auto range = declStatement->getSourceRange();
-//            const auto& sm = *result.SourceManager;
-//            const auto startLoc = sm.getDecomposedLoc(range.getBegin());
-//            const auto endLoc = sm.getDecomposedLoc(range.getEnd());
-//            const auto fileData = sm.getBufferData(startLoc.first);
-//
-//            auto sourceString = fileData.substr(
-//                startLoc.second, endLoc.second - startLoc.second).str();
-//
-//            std::cout << "ok: " <<  sourceString  << std::endl;
-//
-//            rewriter.InsertText(declStatement->getLocStart(), "// decl\n", true, true);
-//        }
-    }
-
-private:
-    Rewriter &rewriter;
-};
-
 class MyASTConsumer final : public ASTConsumer {
 public:
-    MyASTConsumer(Rewriter &rewriter, TypoMan & typosIn)
-        : handlerForIf(rewriter)
-        , handlerForDeclStmt(rewriter)
-        , typos(typosIn)
+    explicit MyASTConsumer(TypoMan & typosIn)
+        : typos(typosIn)
     {
-        matcher.addMatcher(ifStmt().bind("ifStmt"), &handlerForIf);
-        matcher.addMatcher(declStmt().bind("declStmt"), &handlerForDeclStmt);
-        matcher.addMatcher(declStmt().bind("declStmt"), &handlerForDeclStmt);
     }
 
-    void HandleTranslationUnit(ASTContext &context) override
+    void HandleTranslationUnit(ASTContext& context) override
     {
         matcher.matchAST(context);
     }
 
     bool HandleTopLevelDecl(clang::DeclGroupRef d) override
     {
-        for (auto it = d.begin(); it != d.end(); it++)
-        {
-//            {
-//                clang::VarDecl *vd = llvm::dyn_cast<clang::VarDecl>(*it);
-//                if (vd && vd->isFileVarDecl() && !vd->hasExternalStorage()) {
-//                    std::cerr << "Read top-level variable decl: '";
-//                    std::cerr << vd->getDeclName().getAsString();
-//                    std::cerr << std::endl;
-//                }
-//            }
-//            {
-//                auto fd = llvm::dyn_cast<clang::NamedDecl>(*it);
-//                if (fd) {
-//                    std::cerr << "Read top-level variable decl: '";
-//                    std::cerr << fd->getDeclName().getAsString();
-//                    std::cerr << std::endl;
-//                }
-//            }
-//            {
-//                auto fd = llvm::dyn_cast<clang::NamespaceDecl>(*it);
-//                if (fd) {
-//                    std::cerr << "Read top-level variable decl: '";
-//                    std::cerr << fd->getDeclName().getAsString();
-//                    std::cerr << std::endl;
-//                }
-//            }
-//            {
-//                clang::VarDecl *vd = llvm::dyn_cast<clang::VarDecl>(*it);
-//                if (vd) {
-//                    std::cerr << "Read top-level variable decl: '";
-//                    std::cerr << vd->getDeclName().getAsString();
-//                    std::cerr << std::endl;
-//                }
-//            }
-//            {
-//                auto fd = llvm::dyn_cast<clang::FunctionDecl>(*it);
-//                if (fd) {
-//                    std::cerr << "Read top-level variable decl: '";
-//                    std::cerr << fd->getDeclName().getAsString();
-//                    std::cerr << std::endl;
-//                }
-//            }
-
+        for (auto it = d.begin(); it != d.end(); it++) {
             auto fd = llvm::dyn_cast<clang::NamedDecl>(*it);
             if (fd) {
                 std::string identifier = fd->getDeclName().getAsString();
@@ -235,76 +114,24 @@ public:
     }
 
 private:
-    IfStmtHandler handlerForIf;
-    DeclStmtHandler handlerForDeclStmt;
     MatchFinder matcher;
     TypoMan & typos;
 };
 
-class MyPPCallbacks : public clang::PPCallbacks {
-public:
-//    {
-//        std::cout << "======" << std::endl;
-////        auto & ci = getCompilerInstance();
-//        clang::Token token;
-//        do {
-//            assert(ci.hasPreprocessor());
-//            auto & preprocessor = ci.getPreprocessor();
-//            auto & diagnosticsEngine = ci.getDiagnostics();
-////            auto & preprocessor = pp;
-////            auto & diagnosticsEngine = pp.getDiagnostics();
-//            if (diagnosticsEngine.hasErrorOccurred()) {
-//                break;
-//            }
-//            if (!preprocessor.isInPrimaryFile()) {
-//                break;
-//            }
-//            //preprocessor.LexNonComment(token);
-//            preprocessor.Lex(token);
-//            if (token.getKind() == tok::comment) {
-//                // is comment
-//            }
-//            //tok::unknown
-//            //identifier
-//            //raw_identifier
-//            //string_literal
-//            //wide_string_literal
-//            //angle_string_literal
-//            //utf8_string_literal
-//            //utf16_string_literal
-//            //utf32_string_literal
-//            preprocessor.DumpToken(token);
-//            std::cerr << std::endl;
-//        } while (token.isNot(clang::tok::eof));
-//    }
-};
-
 class MyFrontendAction final : public ASTFrontendAction {
 public:
-    MyFrontendAction(TypoMan & typosIn) : typos(typosIn) {}
+    explicit MyFrontendAction(TypoMan & typosIn) : typos(typosIn) {}
 
-    void EndSourceFileAction() override
-    {
-//        rewriter.getEditBuffer(rewriter.getSourceMgr().getMainFileID())
-//            .write(llvm::outs());
-    }
-
-    std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &ci,
-                                                   StringRef file) override
+    std::unique_ptr<ASTConsumer>
+    CreateASTConsumer(CompilerInstance &ci, StringRef file) override
     {
         commentHandler.setFile(file);
-        commentHandler.setRewriter(&rewriter);
         commentHandler.setTypoSet(&typos);
-
         ci.getPreprocessor().addCommentHandler(&commentHandler);
-        ci.getPreprocessor().addPPCallbacks(std::make_unique<MyPPCallbacks>());
-
-        rewriter.setSourceMgr(ci.getSourceManager(), ci.getLangOpts());
-        return std::make_unique<MyASTConsumer>(rewriter, typos);
+        return std::make_unique<MyASTConsumer>(typos);
     }
 
 private:
-    Rewriter rewriter;
     TypoMan & typos;
     MyCommentHandler commentHandler;
 };
@@ -320,7 +147,7 @@ public:
 class MyFrontendActionFactory final : public FrontendActionFactory {
     TypoMan & typos;
 public:
-    MyFrontendActionFactory(TypoMan & typosIn) : typos(typosIn) {}
+    explicit MyFrontendActionFactory(TypoMan & typosIn) : typos(typosIn) {}
 
     clang::FrontendAction* create() override {
         return new MyFrontendAction(typos);
